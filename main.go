a1d5a8a7f58aba84054b5c93adba78d47c08e9e4
MaoDaGreith <traianlupanciuc@gmail.com>
2022-07-30 14:21:32 +0300

    A simple router was made in order to redirect to different page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>This is a title Page</h1>")
+	if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "For more info, please contact me at <a "+
+			"href=\"mailto:traianlupanciuc@gmail.com\">traianlupanciuc@gmail.com</a>")
+	} else if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>This is a title Page</h1>")
+	}
 }
 
 func main() {
