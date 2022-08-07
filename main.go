5610b7af69a8070a7b89caf39dd7cac36bcd6a8f
MaoDaGreith <traianlupanciuc@gmail.com>
2022-08-07 22:48:52 +0300

    Splitted the handlerfunc into smaller funcs

--- main.go
+++ main.go
@@ -6,22 +6,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "For more info, please contact me at <a "+
-			"href=\"mailto:traianlupanciuc@gmail.com\">traianlupanciuc@gmail.com</a>")
-	} else if r.URL.Path == "/" {
-		fmt.Fprintf(w, "<h1>This is a title Page</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>The Page you are looking for does not exist :(</h1>")
-	}
+	fmt.Fprint(w, "<h1>The Page you are looking for does not exist :(</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "For more info, please contact me at <a href=\"mailto:traianlupanciuc@gmail.com\">traianlupanciuc@gmail.com</a>")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
